03d4968af5bab3ac8f55382e9f3bb840ecaa15d3
yanmin <ymchan.dev@gmail.com>
2023-01-21 19:39:35 +0800

    section 2: taking care of test code - eliminating repeated code

--- src/udemy/numbers/BiggerAndSmallerTest.java
+++ src/udemy/numbers/BiggerAndSmallerTest.java
@@ -1,56 +1,54 @@
 package udemy.numbers;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BiggerAndSmallerTest {
+    private BiggerAndSmaller algo;
+
+    @Before
+    public void setup() {
+        algo = new BiggerAndSmaller();
+    }
+
+    // example of a helper method
+    private int[] nums(int... numbers) {
+        return numbers;
+    }
+
+    private void assertBiggestAndSmallest(int biggest, int smallest) {
+        Assert.assertEquals(biggest, algo.getBiggest());
+        Assert.assertEquals(smallest, algo.getSmallest());
+    }
+
     @Test
     public void numbersInIncreasingOrder() {
-        BiggerAndSmaller algo = new BiggerAndSmaller();
-
-        algo.find(new int[] { 2, 3, 4 });
-
-        Assert.assertEquals(4, algo.getBiggest());
-        Assert.assertEquals(2, algo.getSmallest());
+        algo.find(nums(2, 3, 4));
+        assertBiggestAndSmallest(4,2);
     }
 
     @Test
     public void numbersInDecreasingOrder() {
-        BiggerAndSmaller algo = new BiggerAndSmaller();
-
-        algo.find(new int[] { 4, 3, 2 });
-
-        Assert.assertEquals(4, algo.getBiggest());
-        Assert.assertEquals(2, algo.getSmallest());
+        algo.find(nums(4, 3, 2));
+        assertBiggestAndSmallest(4,2);
     }
 
     @Test
     public void numbersInAnyOrder() {
-        BiggerAndSmaller algo = new BiggerAndSmaller();
-
-        algo.find(new int[] { 19, 5, 2, 17, 15, 10 });
-
-        Assert.assertEquals(19, algo.getBiggest());
-        Assert.assertEquals(2, algo.getSmallest());
+        algo.find(nums(19, 5, 2, 17, 15, 10));
+        assertBiggestAndSmallest(19,2);
     }
 
     @Test
     public void onlyOneNumber() {
-        BiggerAndSmaller algo = new BiggerAndSmaller();
-
-        algo.find(new int[] { 16 });
-
-        Assert.assertEquals(16, algo.getBiggest());
-        Assert.assertEquals(16, algo.getSmallest());
+        algo.find(nums(16));
+        assertBiggestAndSmallest(16,16);
     }
 
     @Test
     public void repeatedNumbers() {
-        BiggerAndSmaller algo = new BiggerAndSmaller();
-
-        algo.find(new int[] { 16, 2, 16, 3, 4, 5, 15, 16 });
-
-        Assert.assertEquals(16, algo.getBiggest());
-        Assert.assertEquals(2, algo.getSmallest());
+        algo.find(nums(16, 2, 16, 3, 4, 5, 15, 16));
+        assertBiggestAndSmallest(16,2);
     }
 }
